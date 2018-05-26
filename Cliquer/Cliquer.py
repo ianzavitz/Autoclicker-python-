cb9bb20299aaa576ec5c0156b3b88b3e9961ff15
Ian Zavitz <31973908+ianzavitz@users.noreply.github.com>
2018-05-26 16:18:23 -0500

    Update Cliquer.py

--- Cliquer/Cliquer.py
+++ Cliquer/Cliquer.py
@@ -6,14 +6,27 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("n", help="Number of clicks")
+parser.add_argument("-d", help="Delay between clicks")
 parser.add_argument("--fixed", help="Enable Fixed Click Location", default=False, action='store_const', const=True)
+parser.add_argument("--random",help="Enable Delay Variation/Randomization", default=False, action='store_const', const=True)
 args = parser.parse_args()
 
-n = args.n
+call = ''
 
+#fixed or not?
 if(args.fixed!=True):
-    os.system("python3 Cliquer_Unfixed.py " + n)
+    call+="python3 Cliquer_Unfixed.py " + args.n
 else:
-    os.system("python3 Cliquer_Fixed.py " + n)
+    call+="python3 Cliquer_Fixed.py " + args.n
     
+#base delay specified?
+if(args.d!=None):
+    call+=" -d " + args.d
+
+#varied/randomized delay or not?  
+if(args.random==True):
+    call+=" --random"
+    
+os.system(call)
+
 sys.exit(0)
